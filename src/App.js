6d35e1b62a51bcf661afc1c21317f092b3b8ea58
Alpha <81774909+alpha-Issagha@users.noreply.github.com>
2023-09-26 18:35:28 +0000

    Add and Delete Element

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import "./styles.css";
 
 function App() {
   //state
-  const [compteur, setCompteur] = useState(19);
+  //const [compteur, setCompteur] = useState(19);
 
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Orange" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Mangue" },
+  ]);
   //comportement
   const incremente = () => {
     setCompteur(compteur + 1);
   };
+
+  const deleteFruit = (ind) => {
+    //copie du tab
+    //console.log(ind);
+    const fruitCopy = fruits.slice();
+
+    //modification
+    const fruitCopyUpdated = fruitCopy.filter((fruit) => fruit.id != ind);
+    setFruits(fruitCopyUpdated);
+  };
+
   //render
   return (
     <div>
-      <h1>{compteur}</h1>
-      <button onClick={incremente}>incrementer</button>
+      <h1>Liste des Fruits</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {fruit.nom}
+            <button onClick={() => deleteFruit(fruit.id)}>x</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
